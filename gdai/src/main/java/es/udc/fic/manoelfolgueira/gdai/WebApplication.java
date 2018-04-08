0aa5b31585fe5895c631471b7732a431ddbbf45b
Manoel Antón Folgueira Hernández <manoel.folgueira@udc.es>
2018-04-08 13:07:48 +0200

    Base del proyecto.

---
+++ gdai/src/main/java/es/udc/fic/manoelfolgueira/gdai/WebApplication.java
@@ -0,0 +1,13 @@
+package es.udc.fic.manoelfolgueira.gdai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
---
+++ gdai/src/main/java/es/udc/fic/manoelfolgueira/gdai/model/entities/UserRepository.java
@@ -0,0 +1,9 @@
+package es.udc.fic.manoelfolgueira.gdai.model.entities;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
