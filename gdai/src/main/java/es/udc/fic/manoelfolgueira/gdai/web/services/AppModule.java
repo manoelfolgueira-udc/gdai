c0fc346bc7022df7e30381513c3fdc804faacbf5
Manoel Antón Folgueira Hernández <manoel.folgueira@udc.es>
2018-07-02 22:24:12 +0200

    + Soporte de Idioma. Pendiente mejorar configuración desde BD. + Mejoras en el navbar. Falta arreglar el problema del display:none; automático cuando se pincha fuera de cualquier dropdown menu

--- gdai/src/main/java/es/udc/fic/manoelfolgueira/gdai/web/services/AppModule.java
+++ gdai/src/main/java/es/udc/fic/manoelfolgueira/gdai/web/services/AppModule.java
@@ -3,6 +3,8 @@ package es.udc.fic.manoelfolgueira.gdai.web.services;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 
+import es.udc.fic.manoelfolgueira.gdai.web.util.AvailableLanguages;
+
 
 /**
  * This module is automatically included as part of the Tapestry IoC Registry,
@@ -14,7 +16,9 @@ public class AppModule {
     public static void contributeApplicationDefaults(
         MappedConfiguration<String, String> configuration) {
 
-        configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
+    	AvailableLanguages.initialize();
+        configuration.add(SymbolConstants.SUPPORTED_LOCALES, 
+        		AvailableLanguages.getCodes());
 
     }
 
