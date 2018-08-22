e7217870248bc3a74e58dfa45027e2043cd23f13
Manoel Antón Folgueira Hernández <manoel.folgueira@udc.es>
2018-08-22 16:27:55 +0200

    Cambiando datos del usuario de pruebas principal

--- gdai/src/sql/2-MySQLCreateData.sql
+++ gdai/src/sql/2-MySQLCreateData.sql
@@ -14,7 +14,7 @@ values('GP', NOW(), NULL);
     
 -- Users
 insert into gdai_user(loginName, encryptedPassword, firstName, lastName, email, phoneNumber, avatarUrl, gender, hireDate, dateOfBirth, expirationtime, groupId)
-values('b', 'AZ0OZU/oardxM', 'b', 'b', 'b@b.com', '1456', 'https://vignette.wikia.nocookie.net/clubpenguin/images/e/e2/Smiley-300x300.png/revision/latest?cb=20090401005140', 'M', NOW(), NOW(), '2020-08-05 18:19:03', 1);
+values('Manoel', 'AZ0OZU/oardxM', 'Manoel', 'Folgueira', 'manoel.folgueira@gdai.com', '1456', 'https://vignette.wikia.nocookie.net/clubpenguin/images/e/e2/Smiley-300x300.png/revision/latest?cb=20090401005140', 'M', NOW(), NOW(), '2020-08-05 18:19:03', 1);
 
 insert into gdai_user(loginName, encryptedPassword, firstName, lastName, email, phoneNumber, avatarUrl, gender, hireDate, dateOfBirth, expirationtime, groupId)
 values('a1', 'AZ0OZU/oardxM', 'b', 'b', 'b@b.com', '1456', 'https://vignette.wikia.nocookie.net/clubpenguin/images/e/e2/Smiley-300x300.png/revision/latest?cb=20090401005140', 'M', NOW(), NOW(), NOW(), 2);
